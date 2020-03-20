9edb4d113958584533151daafa2337665c190071
r a f t <hakan.eryargi@gmail.com>
2020-03-20 12:20:40 +0100

    admin approve/reject project. json response to errors.

--- rest-api/src/main/java/org/madb/api/controller/AdminController.java
+++ rest-api/src/main/java/org/madb/api/controller/AdminController.java
@@ -2,13 +2,20 @@ package org.madb.api.controller;
 
 import java.util.List;
 
+import org.madb.api.controller.exception.NotFoundException;
 import org.madb.api.jpa.ProjectRepository;
 import org.madb.api.model.Project;
+import org.madb.api.model.Project.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @RestController
 @RequestMapping("/v1")
@@ -23,8 +30,33 @@ public class AdminController {
 		return projectRepository.findByStatus(Project.Status.PENDING);
 	}
 
-	
-	static class Response {
+	@PutMapping("/admin/approve_project/{id}")
+	Response approveProject(@PathVariable Integer id) {
 		
+	    return projectRepository.findById(id)
+	    	.map(project -> {
+				project.setStatus(Status.APPROVED);
+				projectRepository.save(project);
+	    		return new Response("OK");
+	    	})
+	    .orElseThrow(() -> new NotFoundException(id));
+	}
+	
+	@PutMapping("/admin/reject_project/{id}")
+	Response rejectProject(@PathVariable Integer id) {
+		
+	    return projectRepository.findById(id)
+	    	.map(project -> {
+				project.setStatus(Status.REJECTED);
+				projectRepository.save(project);
+	    		return new Response("OK");
+	    	})
+	    .orElseThrow(() -> new NotFoundException(id));
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class Response {
+		String status;
 	}
 }
--- rest-api/src/main/java/org/madb/api/controller/exception/ExceptionHandlerAdvice.java
+++ rest-api/src/main/java/org/madb/api/controller/exception/ExceptionHandlerAdvice.java
@@ -1,5 +1,8 @@
 package org.madb.api.controller.exception;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,12 +10,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-class NotFoundAdvice {
+class ExceptionHandlerAdvice {
 
 	@ResponseBody
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	String employeeNotFoundHandler(NotFoundException ex) {
-		return ex.getMessage();
+    Map<String, String> notFound(Exception e) {
+        return Collections.singletonMap("message", e.getMessage());
+    }	
+	
+	@ResponseBody
+	@ExceptionHandler(BadRequestException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	Map<String, String> employeeNotFoundHandler(BadRequestException e) {
+        return Collections.singletonMap("message", e.getMessage());
 	}
+	
 }
