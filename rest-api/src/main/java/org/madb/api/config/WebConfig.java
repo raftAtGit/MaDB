c2ebe15f4a6a5f9a410bcf17a06a35f8732c9158
r a f t <hakan.eryargi@gmail.com>
2020-03-30 17:19:39 +0200

    published javadocs at /javadoc endpoint

--- rest-api/src/main/java/org/madb/api/config/WebConfig.java
+++ rest-api/src/main/java/org/madb/api/config/WebConfig.java
@@ -3,20 +3,29 @@ package org.madb.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/** Enables CORS (Cross-Origin Resource Sharing). */
+/** Web MVC configuration. Enables CORS (Cross-Origin Resource Sharing) and publishes javadocs. */
 @Configuration
-public class CorsConfig {
+public class WebConfig {
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
+		
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
                 		.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 			}
+			
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			    registry
+			      .addResourceHandler("/javadoc/**")
+			      .addResourceLocations("classpath:/static/javadoc/");
+			 }
 		};
 	}
 }
