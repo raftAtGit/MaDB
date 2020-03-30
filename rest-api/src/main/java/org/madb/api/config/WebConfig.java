85c6e23bf6070e47cdf842572e979b7ec109dc09
Hakan Eryargi <raftAtGit@users.noreply.github.com>
2020-03-30 18:59:20 +0200

    Feature/javadoc (#5)
    fixed /javadoc/ endpoint.

--- rest-api/src/main/java/org/madb/api/config/WebConfig.java
+++ rest-api/src/main/java/org/madb/api/config/WebConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
 
 /** Web MVC configuration. Enables CORS (Cross-Origin Resource Sharing) and publishes javadocs. */
 @Configuration
@@ -14,18 +16,29 @@ public class WebConfig {
 	public WebMvcConfigurer corsConfigurer() {
 		
 		return new WebMvcConfigurer() {
+			/** Enable CORS */
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
                 		.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 			}
 			
+			/** Publish javadocs at /javadoc path */
 			@Override
 			public void addResourceHandlers(ResourceHandlerRegistry registry) {
 			    registry
 			      .addResourceHandler("/javadoc/**")
-			      .addResourceLocations("classpath:/static/javadoc/");
-			 }
+			      .addResourceLocations("classpath:/javadoc/")
+			      .setCachePeriod(3600)
+			      .resourceChain(true)
+			      .addResolver(new EncodedResourceResolver());
+			}
+			
+			/** serve /javadoc/ path from /javadoc/index.html */
+			@Override
+		    public void addViewControllers(ViewControllerRegistry registry) {
+		        registry.addViewController("/javadoc/").setViewName("forward:/javadoc/index.html");
+		    }
 		};
 	}
 }
